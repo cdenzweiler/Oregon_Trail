e57a1cecfa1d4a2512a02114ae105839a0336bc5
Chris Enzweiler <cdenzweiler@gmail.com>
2020-12-07 11:36:04 -0500

    Tests completed

--- main.js
+++ main.js
@@ -1,74 +1,60 @@
-console.log("november something...")
-
-
-
-
-
+console.log("December seventh...")
 class Traveler {
-    constructor(name, food, isHealthy,) {
-        this.name = name;
+    constructor(name1) {
+        this.name = name1;
         this.food = 1;
         this.isHealthy = true
     }
 
     hunt() {
         this.food += 2;
-        document.body.innerHTML = (this.name + " returned with more food. " + this.name + " now has "+ this.food + " remaining");
     };
 
     eat() {
 
         if (this.food < 1) {
             this.isHealthy = false;
-            document.body.innerHTML = (this.name + " has no more food! Now " + this.name + " is sick.")
         } else {
-        this.food -= 1;
-        document.body.innerHTML = (this.name + " ate some food and now has: " + this.food + " remaining.")
+            this.food -= 1;
         }
     }
 }
 
-
-const Debbie = new Traveler("Debbie")
-
 class Wagon {
-    constructor(capacity, []) {
-        this.capacity = 5;
+    constructor(capacity) {
+        this.capacity = capacity
         this.passengers = [];
-    };
-
+    }
     getAvailableSeatCount() {
-        return this.capacity
-    };
-
+        return this.capacity - this.passengers.length
+    }
     join(name) {
-        if (this.capacity >= 1) {
-            this.capacity -= 1;
+        if (this.getAvailableSeatCount() >= 1) {
+
             this.passengers.push(name)
-            console.log(name + " has joined the wagon.")
+            console.log(Traveler.name + " has joined the wagon.")
         } else (
             console.log("No seats available")
         )
-        return document.body.innerHTML = (this.capacity + " seats remain on the wagon.")
-    };
-
+    }
     shouldQuarantine() {
-        if (passengers.isHealthy == false) {
-            return true
 
-        } else {
-           return false
+        for (let i = 0; i < this.passengers.length; i++) {
+
+            if (this.passengers[i].isHealthy == true) {
+            } else {
+                return true
+            }
+        }
+        return false
+    }
+    totalFood() {
+
+        let foodCount = 0;
+        for (let i = 0; i < this.passengers.length; i++) {
+            foodCount += this.passengers[i].food
 
         }
-        
-    };
-    totalFood() {
-        
-        
+        return foodCount
     }
 }
-
-let Freedom = new Wagon(5, [])
-// console.log(Freedom.numWheels)
-Freedom.join(Debbie)
-
